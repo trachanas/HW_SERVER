049a344da56c0abafa9f3627c1135734bb492fd3
trachanios giorgos <sdi1300178@di.uoa.gr>
2020-12-20 13:13:06 +0200

    SIGTSTP ignore

--- server.c
+++ server.c
@@ -95,7 +95,7 @@ int main(int argc, char **argv) {
     }
 
     signal(SIGINT, signal_handler);
-
+    signal(SIGTSTP, SIG_IGN);
     while (1){
 
         fd = accept(mySocket, clientPtr, &socklen);
